5984c59fdf04f2b1c0678410eea9612421725167
surekhamanne7 <149230799+surekhamanne7@users.noreply.github.com>
2023-10-28 13:54:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region      = "us-east-1"
-    access_key  = "AKIAR6XUM6ISH7D524MD"
-    secret_key  = "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"
 }
 
 resource "aws_instance" "my_ec2" {
