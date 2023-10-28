07d5bf7e4c776b39245eecafc76b18f6bc6e284b
surekhamanne7 <149230799+surekhamanne7@users.noreply.github.com>
2023-10-28 13:48:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,14 @@
 provider "aws" {
-    region = "us-east-1"
+    region      = "us-east-1"
+    access_key  = "AKIAR6XUM6ISH7D524MD"
+    secret_key  = "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"
 }
 
 resource "aws_instance" "my_ec2" {
-    ami = "ami-0df435f331839b2d6"
-    instance_type = "t2.micro"
+    ami             =  "ami-03a6eaae9938c858c"
+    instance_type   =  "t2.micro"
+
+    tags = {
+        Name = "my-first-ec2"
+    }
 }
